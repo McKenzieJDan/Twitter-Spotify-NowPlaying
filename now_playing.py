c4f693120bf761cdb28b0aab2ce89a333680da25
McKenzieJDan <mckenziejdan@gmail.com>
2018-01-11 13:44:28 +0000

    Clean up.

--- now_playing.py
+++ now_playing.py
@@ -57,22 +57,17 @@ def main():
                     playlist_id = spotify_playlist
                     track_uri = results['item']['uri']
                     track_uri_latest = [track_uri]
-#                    with open('file.txt', 'a') as file:
-#                        now = datetime.datetime.now()
-#                        timestamp = now.strftime("[%Y-%m-%d %H:%M]")
-#                        file.writelines("\n" + timestamp + " " + artist_name + " - " + track_name)
                     try:
                         #api.update_status(tweet_text)
                         #api.update_profile_image(profile_image)
                         #api.update_profile_banner(profile_image)
-                        #sp.user_playlist_add_tracks(username, playlist_id, track_uri_latest, position=0)
                         update = True
                         print "updated"
                     except tweepy.error.TweepError:
                         update = False
                         pass
                     if update == True:
-                        sp.user_playlist_add_tracks(username, playlist_id, track_uri_latest, position=0)
+                        #sp.user_playlist_add_tracks(username, playlist_id, track_uri_latest, position=0)
 
                         # Database work
                         sqlite_file = 'now_playing.db'
@@ -80,7 +75,6 @@ def main():
                         c = conn.cursor()
 
                         table_name = 'nowplaying'
-                        track_name = track_name
                         artist_name = artist_name
                         album_name = results['item']['album']['name']
 
@@ -89,58 +83,21 @@ def main():
                         column_three = 'album'
                         column_four = 'totalPlays'
 
-                        # Check if exists
-                        #c.execute("SELECT track FROM nowplaying WHERE track=?"), (track_name)).\
-
-                        #c.execute('create table components (rowid int,name varchar(50))')
-                        #c.execute('insert into components values(?,?)', (1,'foo',))
-
-                        for name in str(track_name):
-                            c.execute("SELECT track FROM components WHERE name = ?", (name,))
-                            data=c.fetchone()
-                            if data is None:
-                                print('There is no component named ''ame)
+                        for name in (track_name):
+                            c.execute("SELECT count(*) FROM nowplaying WHERE track = ?", (name,))
+                            data=c.fetchone()[0]
+                            if data==0:
+                                #print 'Doesnt exist insert into databse'
+                                print('There is no component named %s'%name)
+                                #c.execute("INSERT OR IGNORE INTO {tn} ({c1}, {c2}, {c3}, {c4}) VALUES ('{a1}', '{a2}', '{a3}', 1)".\
+                                #    format(tn=table_name, c1=column_one, c2=column_two, c3=column_three, c4=column_four, a1=track_name, a2=artist_name, a3=album_name))
                             else:
-                                print('Component %s found with rowid %s')
-
-                        #if not exists:
-                        #    print "not exists"
-                        #    c.execute("INSERT OR IGNORE INTO {tn} ({c1}, {c2}, {c3}, {c4}) VALUES ('{a1}', '{a2}', '{a3}', 1)".\
-                        #        format(tn=table_name, c1=column_one, c2=column_two, c3=column_three, c4=column_four, a1=track_name, a2=artist_name, a3=album_name))
-                        #else:
-                        #    pass
-                        #    print "exists"
-
-
-
-                        #c.execute("INSERT OR IGNORE INTO {tn} ({c4) VALUES ('{a1}', '{a2}', '{a3}')".\
-                        #    format(tn=table_name, c4=column_foure, a4=track_name, a2=artist_name, a3=album_name))
-
-                        #UPDATE TableName SET TableField = TableField + 1 WHERE SomeFilterField = @ParameterID
-
-
-                        # A) Inserts an ID with a specific value in a second column
-                        #try:
-                        #    c.execute("INSERT INTO {tn} ({idf}, {cn}) VALUES (123456, 'test')".\
-                        #    format(tn=table_name, idf=id_column, cn=column_name))
-                        #    print "hi"
-                        #except sqlite3.IntegrityError:
-                        #    print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
-
-                        # B) Tries to insert an ID (if it does not exist yet)
-                        # with a specific value in a second column
-                        #c.execute("INSERT OR IGNORE INTO {tn} ({idf}, {cn}) VALUES (1232456, 'test')".\
-                        #    format(tn=table_name, idf=id_column, cn=column_name))
-
-                        # UPDATE Products SET Price = Price + 50 WHERE ProductID = 1
-
-                        # C) Updates the newly inserted or pre-existing entry
-                        #    c.execute("UPDATE {tn} SET {cn}=('Hi World') WHERE {idf}=(123456)".\
-                        #    format(tn=table_name, cn=column_name, idf=id_column))
-
+                                #print 'Does exist updating play count'
+                                print name
+                                print('Component %s found in %s row(s)'%(name,data))
+                                c.execute("UPDATE nowplaying SET totalPlays = totalPlays + 1 WHERE track = ?", (name,))
                         conn.commit()
                         conn.close()
-
                     else:
                         pass
                     last_id = track_id
